dfe1c9b4fbd00394a26a9368568a8bd5dc4d656a
Michael <gainey.mic@gmail.com>
2022-05-20 02:50:48 -1000

    added routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './layout/App';
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(<App />,document.getElementById('root'));
--- frontend/src/layout/App.jsx
+++ frontend/src/layout/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import '../styles/App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import Home from '../pages/Home'
 
 const App = () => {
   return (
-    <div>Hello World!</div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+    </Router>
   );
 };
 
