ef48abc1a7698d71a5dc0da16b6501727a47c4b9
Michael <gainey.mic@gmail.com>
2022-05-21 03:46:04 -1000

    eslint

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './layout/App';
-import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(<App />,document.getElementById('root'));
